8ed63a92d532e011575630da963d331dfbc64125
Silviu Bocancea <silviuboc4@gmail.com>
2020-05-31 15:33:04 -0700

    header and header commponents added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
-import "./App.css";
-import ShopPage from "./components/pages/shop/shop.component";
-
-import HomePage from "./components/pages/homepage/homepage.component";
-import { Route, Switch } from "react-router";
+import React from 'react';
+import './App.css';
+import ShopPage from './components/pages/shop/shop.component';
+import Header from './components/header/header.component';
+import HomePage from './components/pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router';
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
