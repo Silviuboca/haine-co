554b37dfbb3d74d63e684f45a7f4ffc6d9f95e3e
Silviu Bocancea <silviuboc4@gmail.com>
2020-05-31 12:28:47 -0700

    Added router, and routing components to menu-items

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 
 import HomePage from "./components/pages/homepage/homepage.component";
+import { Route, Switch } from "react-router";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
