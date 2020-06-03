be1cb84395e4a7828d1b4c3627031090f2e827b8
Silviu Bocancea <silviuboc4@gmail.com>
2020-06-02 21:44:13 -0700

    implemeted firebase utils, able to store and authenticate users into firestore db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,39 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyAoKNNQpGJMmSxo1nq3dtOE_70as3nXkQA",
-  authDomain: "haine-db.firebaseapp.com",
-  databaseURL: "https://haine-db.firebaseio.com",
-  projectId: "haine-db",
-  storageBucket: "haine-db.appspot.com",
-  messagingSenderId: "1035428579411",
-  appId: "1:1035428579411:web:966408a07e62f98a2b1ea6",
-  measurementId: "G-C0HCVS68JE"
+  apiKey: 'AIzaSyAoKNNQpGJMmSxo1nq3dtOE_70as3nXkQA',
+  authDomain: 'haine-db.firebaseapp.com',
+  databaseURL: 'https://haine-db.firebaseio.com',
+  projectId: 'haine-db',
+  storageBucket: 'haine-db.appspot.com',
+  messagingSenderId: '1035428579411',
+  appId: '1:1035428579411:web:966408a07e62f98a2b1ea6',
+  measurementId: 'G-C0HCVS68JE',
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        additionalData,
+      });
+    } catch (err) {
+      console.log('error creating user', err.message);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
@@ -19,7 +44,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({  prompt: 'select_account' });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
